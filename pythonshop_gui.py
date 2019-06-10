7e04f85a7076ce29b34b544fba2b70e281dc2e5f
karmelkaa <50425976+karmelkaa@users.noreply.github.com>
2019-06-10 19:45:36 +0200

    Skaluj
    Przypisałam funkcję guzikowi.

--- pythonshop_gui.py
+++ pythonshop_gui.py
@@ -37,7 +37,7 @@ photo_space.grid(column=0, rowspan=6)
 
 sidebar=Frame(root, width=100).grid(column=1)
 obroc_sidebar=Button(sidebar, text="Obróć").grid(row=0, column=1, sticky="nesw")
-skaluj_sidebar=Button(sidebar, text="Skaluj").grid(row=1, column=1, sticky="nesw")
+skaluj_sidebar=Button(sidebar, text="Skaluj", command=skaluj).grid(row=1, column=1, sticky="nesw")
 czarno_bialy_sidebar=Button(sidebar, text="Czarno-biały").grid(row=2, column=1, sticky="nesw")
 spacer_sidebar=Frame(sidebar, height=300).grid(row=3, column=1, sticky="nesw")
 
